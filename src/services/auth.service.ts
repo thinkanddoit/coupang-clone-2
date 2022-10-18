028427cbb2b5836a7aba7aa758f794c99b7fa08f
easyone <thinkanddoit@naver.com>
2022-10-19 01:01:42 +0900

    #1 refactor: src/services 클래스들을 추상화된 부모클래스 상속하도록 변경 및 반영

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { AxiosUtil, TokenUtil } from "../utils";
+import Service from "./service";
 
 type SignupAgreements = {
   /** 만 14세 이상입니다 */
@@ -21,7 +22,7 @@ type SignupAgreements = {
   agree_to_receive_push: boolean;
 };
 
-class AuthService {
+class AuthService extends Service {
   /** accessToken 1일, refreshToken 7일로 설정하는 공통 로직, 함수화 */
   private setAllToken(data: any) {
     TokenUtil.set("access", data.access, 1);
@@ -30,12 +31,9 @@ class AuthService {
 
   /** refreshToken을 이용해 새로운 토큰을 발급받습니다. */
   async refresh() {
-    const refreshToken = TokenUtil.get("refresh");
-    if (!refreshToken) {
-      return;
-    }
-    AxiosUtil.setDefaultHeader(refreshToken);
+    super.setAxiosDefaultHeader("refresh");
     const { data } = await AxiosUtil.post("/auth/refresh", null);
+
     this.setAllToken(data);
   }
 
@@ -54,12 +52,14 @@ class AuthService {
       phoneNumber,
       agreements,
     });
+
     this.setAllToken(data);
   }
 
   /** 이미 생성된 계정의 토큰을 발급받습니다. */
   async login(email: string, password: string) {
     const { data } = await AxiosUtil.post("/auth/login", { email, password });
+
     this.setAllToken(data);
   }
 }
