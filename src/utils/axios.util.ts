71a5b6e5e0fe07ef4909123c7e41b59cd8724d06
easyone <thinkanddoit@naver.com>
2022-10-19 13:47:15 +0900

    #1 fix: axios default header 적용안되는 버그 수정
    - instance를 통한 호출 방식에서 직접 호출로 변경

--- src/utils/axios.util.ts
+++ src/utils/axios.util.ts
@@ -1,32 +1,30 @@
 import axios from "axios";
 
-class AxiosUtil {
-  private instance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_HOST,
-  });
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_HOST;
 
+class AxiosUtil {
   setDefaultHeader(AUTH_TOKEN: string) {
     axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
   }
 
   get(url: string) {
-    return this.instance.get(url);
+    return axios.get(url);
   }
 
   post(url: string, data: any) {
-    return this.instance.post(url, data);
+    return axios.post(url, data);
   }
 
   delete(url: string) {
-    return this.instance.delete(url);
+    return axios.delete(url);
   }
 
   put(url: string, data: any) {
-    return this.instance.put(url, data);
+    return axios.put(url, data);
   }
 
   patch(url: string, data: any) {
-    return this.instance.patch(url, data);
+    return axios.patch(url, data);
   }
 }
 
