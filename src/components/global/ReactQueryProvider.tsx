068536d5450f29567a37d3ae1ef19cfab9f9b6a0
easyone <thinkanddoit@naver.com>
2022-10-25 15:55:07 +0900

    #4 feat: query defaultOptions 설정

--- src/components/global/ReactQueryProvider.tsx
+++ src/components/global/ReactQueryProvider.tsx
@@ -2,7 +2,21 @@ import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 function ReactQueryProvider({ children, dehydratedState }: any) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 300000, // 5 minutes
+            cacheTime: 900000, // 15 minutes
+            retry: false,
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
